decd95b3e05fe853f2d086e758962cc80c2cb1a3
DESKTOP-MFVGBGK\user <StandOut.first@gmail.com>
2025-05-29 15:03:56 +0900

    3. 지오메트리(Geometry) part-2

--- workspace/study/3. 지오메트리(Geometry)/02-geometry.js
+++ workspace/study/3. 지오메트리(Geometry)/02-geometry.js
@@ -74,7 +74,33 @@ class App{
 
     _setupModel() {
         // BoxGeometry(가로, 세로, 높이, 각각에 대한 분할 수 있는 세그먼트의 수)
-        const geometry = new Three.BoxGeometry(1, 1, 1, 2, 2, 2); // 세그먼트 수를 늘려서 더 많은 면을 생성해보기
+        // const geometry = new Three.BoxGeometry(1, 1, 1, 2, 2, 2); // 세그먼트 수를 늘려서 더 많은 면을 생성해보기
+
+        // 원판
+        // const geometry = new Three.CircleGeometry(0.9, 8, 0, Math.PI); // CircleGeometry(반지름, 분할개수,시작각도, 끝각도)
+
+        // 원뿔
+        // const geometry = new Three.ConeGeometry(0.5, 1.6, 16, 9, true, 0, Math.PI); // ConeGeometry(반지름, 높이, 분할개수, 높이분할개수, 원뿔밑면을 open할것인가, 시작각도, 끝각도)
+
+        // 막대
+        // const  geometry = new Three.CylinderGeometry(0.9, 0.7, 1, 15, 3, true, 0, Math.PI); // CylinderGeometry(위쪽반지름, 아래쪽반지름, 높이, 분할개수, 높이분할개수, 원통밑면을 open할것인가, 시작각도, 끝각도)
+
+        // 원   
+        // const geometry = new Three.SphereGeometry(0.8, 32, 16, 0, Math.PI, 0, Math.PI/2); // SphereGeometry(반지름, 수직 세그먼트수, 수평 세그먼트수, 수직 시작각도, 수직끝각도, 수평 시작각도, 수평 끝각도)
+
+        // 링
+        // const geometry = new Three.RingGeometry(0.2, 0.9, 32, 3, 0, Math.PI); // RingGeometry(내부반지름, 외부반지름, 분할개수, 높이분할개수, 시작각도, 끝각도)
+
+        // 사각형
+        // const geometry = new Three.PlaneGeometry(1, 1, 2, 2); // PlaneGeometry(가로, 세로, 가로분할개수, 세로분할개수)
+
+        // 3차원 반지 - 원통형
+        // const geometry = new Three.TorusGeometry(0.8, 0.3, 10, 36, Math.PI); // TorusGeometry(반지름, 두께, 분할개수(표면각), 분할개수(링의 각), 시작각도)
+
+        // 3차원 반지 - 매듭
+        // TorusKnotGeometry는 매듭 모양의 3D 기하학을 생성합니다. 멋진 모양과 다르게 활용도가 아주 낮다.
+        const geometry = new Three.TorusKnotGeometry(0.6, 0.1, 100, 16, 3, 4); // TorusKnotGeometry(반지름, 두께, 분할개수(표면각), 분할개수(링의 각), 매듭의 수, 꼬임의 수)
+        
 
         const material = new Three.MeshStandardMaterial({ color: 0x515151 });  // 색상변경
         const cube = new Three.Mesh(geometry, material); 
