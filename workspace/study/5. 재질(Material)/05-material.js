90c11394fe97b7acb1cc48554cfd5dafcfecb580
DESKTOP-MFVGBGK\user <StandOut.first@gmail.com>
2025-06-10 15:28:05 +0900

    5. 재질(Material) part-2

--- workspace/study/5. 재질(Material)/05-material.js
+++ workspace/study/5. 재질(Material)/05-material.js
@@ -43,7 +43,7 @@ class App{
         const height = this._divContainer.clientHeight; 
 
         const camera = new Three.PerspectiveCamera(75, width / height, 0.1, 100); 
-        camera.position.z = 7; 
+        camera.position.z = 3; 
         this._camera = camera; 
     }
 
@@ -93,17 +93,20 @@ class App{
 
      _setupModel() {
 
+        
+        // 05 material part 1 - start
+
         // vertices는 4개의 정점으로 구성된 사각형을 정의합니다.
-        const vertices = [
-            -1, 1, 0, 
-            1, 1, 0, 
-            -1, -1, 0, 
-            1, -1, 0
-        ];
+        // const vertices = [
+        //     -1, 1, 0, 
+        //     1, 1, 0, 
+        //     -1, -1, 0, 
+        //     1, -1, 0
+        // ];
 
         // Three.BufferGeometry를 사용하여 선을 생성합니다.
-        const geometry = new Three.BufferGeometry();
-        geometry.setAttribute('position', new Three.Float32BufferAttribute(vertices, 3));
+        // const geometry = new Three.BufferGeometry();
+        // geometry.setAttribute('position', new Three.Float32BufferAttribute(vertices, 3));
 
         // 선의 재질을 정의합니다.
         // LineBasicMaterial: 기본 선 재질
@@ -120,21 +123,87 @@ class App{
 
         // LineDashedMaterial: 대시선 재질
         // 선의 길이를 참조해서 재질이 적용됨으로 선의 길이를 계산해 줘야한다.
-        const material =  new Three.LineDashedMaterial({
-            color: 0x00ff00, // 선의 색상
-            dashSize: 0.1, // 대시 크기
-            gapSize: 0.1, // 간격 크기
-            scale: 1 // 대시와 간격의 크기를 조정하는 스케일
+        // const material =  new Three.LineDashedMaterial({
+        //     color: 0x00ff00, // 선의 색상
+        //     dashSize: 0.1, // 대시 크기
+        //     gapSize: 0.1, // 간격 크기
+        //     scale: 1 // 대시와 간격의 크기를 조정하는 스케일
+        // });
+        // const line = new Three.Line(geometry, material);
+        // line.computeLineDistances(); // 대시선 재질을 사용하기 위해 선의 길이를 계산
+
+
+
+        // this._scene.add(line);
+
+        
+        // 05 material part 1 - end
+
+
+
+
+
+
+
+        // 05 material part 2 - start
+        
+        // mesh는 Three.js에서 3D 객체를 생성하는 기본 단위입니다.
+        // 재질(Material)과 지오메트리(Geometry)를 결합하여 3D 객체를 만듭니다.
+        
+        // Mesh에서 가장 많이 사용되는 재질은 MeshStandardMaterial, MeshPhysicalMaterial이다. 상대적으로 속도느 느리나 고품질이다.
+        const material = new Three.MeshPhysicalMaterial({
+        // const material = new Three.MeshStandardMaterial({
+        // const material = new Three.MeshPhongMaterial({
+        // const material = new Three.MeshLambertMaterial({
+        // const material = new Three.MeshBasicMaterial({
+            visible: true, // 재질이 보이는지 여부
+            transparent: false, // 재질이 투명한지 여부
+            opacity: 1.0, // 재질의 투명도 (0.0 ~ 1.0)
+
+            // depth Buffer = 깊이버퍼, z버퍼, 객체의 깊이 정보를 저장하는 버퍼, 0과 1사이 이값이 작을수록 카메라에 가까워짐
+            // MeshBasicMaterial은 기본 재질로, 조명 효과를 받지 않아 상세속성 변화를 크게 느낄 수 없습니다. 
+            depthTest: true, // 깊이 테스트를 활성화
+            depthWrite: true, // 깊이 쓰기를 활성화
+            side: Three.DoubleSide, // 양면 렌더링
+            // side: Three.FrontSide, // 정면만 렌더링
+
+            color: "#d25383", // 재질의 색상
+            emissive: 0x555500, // 방출 색상 (조명 효과를 받지 않는 부분의 색상) 입체감을 주기 좋음!
+            specular : 0xffff00, // 반사 색상 (빛을 반사하는 부분의 색상 빛나는 부분의 색상을 지정)
+            shininess: 10, // 광택도 (0 ~ 1000) 값이 클수록 빛나는 부분이 작아짐
+
+            // MeshStandardMaterial - start
+            roughness: 0.3, // 거칠기 (0.0 ~ 1.0) 값이 작을수록 표면이 매끄럽고 빛나는 부분이 넓어짐
+            metalness: 0.5, // 금속성 (0.0 ~ 1.0) 값이 클수록 금속 재질처럼 보임
+            // MeshStandardMaterial - end
+
+            // MeshPhysicalMaterial - start
+            roughness: 1, // 거칠기 (0.0 ~ 1.0) 값이 작을수록 표면이 매끄럽고 빛나는 부분이 넓어짐
+            metalness: 0, // 금속성 (0.0 ~ 1.0) 값이 클수록 금속 재질처럼 보임
+            clearcoat: 0.5, // 클리어코트 (0.0 ~ 1.0) 값이 클수록 표면에 유리처럼 투명한 코팅 효과가 적용됨
+            clearcoatRoughness: 0.1, // 클리어코트 거칠기 (0.0 ~ 1.0) 값이 작을수록 표면이 매끄럽고 빛나는 부분이 넓어짐
+            // MeshPhysicalMaterial - end
+
+            wireframe: false, // 와이어프레임 모드로 렌더링  
+            // transparent: true, // 투명도 설정
+            // opacity: 0.5 // 투명도 값 (0.0 ~ 1.0)
         });
-        const line = new Three.Line(geometry, material);
-        line.computeLineDistances(); // 대시선 재질을 사용하기 위해 선의 길이를 계산
 
+        const box = new Three.Mesh(
+            new Three.BoxGeometry(1, 1, 1), // 박스 지오메트리
+            material // 재질
+        );
+        box.position.set(-1, 0, 0); // 박스 위치 설정
+        this._scene.add(box); // 박스를 씬에 추가
 
+        const sphere = new Three.Mesh(
+            new Three.SphereGeometry(0.7, 32, 32), material
+        );
+        sphere.position.set(1, 0, 0); // 구 위치 설정
+        this._scene.add(sphere); // 구를 씬에 추가
+        // 이 예제에서는 cube를 사용하지 않음
 
-
-
-
-        this._scene.add(line);
+        // 05 material part 1 - end
         
     }
 
